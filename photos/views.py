be255e604a971602ad7c65ae726a0ded368033df
kunal <kunalbhatia868@gmail.com>
2021-10-31 15:58:00 +0530

    adding a image form added

--- photos/views.py
+++ photos/views.py
@@ -1,13 +1,46 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from photos import models
 # Create your views here.
 
 def gallery(request):
-    return render(request,"photos/gallery.html")
+    categories=models.Category.objects.all()
+    photos=models.Photo.objects.all()
+
+    context={
+    'categories':categories,
+    'photos':photos,
+    }
+
+    return render(request,"photos/gallery.html",context)
 
 def viewPhoto(request,pk):
-    return render(request,"photos/photo.html")
+    photo=models.Photo.objects.get(id=pk)
+    context={
+        'photo':photo,
+    }
+    return render(request,"photos/photo.html",context)
 
 
 def addPhoto(request):
-    return render(request,"photos/addPhoto.html")    
+    categories=models.Category.objects.all()
+    if request.method=='POST':
+        data=request.POST
+        image=request.FILES.get('image') 
+        if data['category'] != 'none' :
+            category=models.Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category,created=models.Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category=None
+
+        photo=models.Photo.objects.create(
+            description=data['description'],
+            category=category,
+            image=image
+        )
+        return redirect('gallery')
+        
+    context={
+    'categories':categories,
+    }
+    return render(request,"photos/addPhoto.html",context)    
